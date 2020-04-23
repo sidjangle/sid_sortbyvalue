59962d0d07d6db89ebc434d87d2f42ade458e42d
sidjangle <61905481+sidjangle@users.noreply.github.com>
2020-04-23 14:55:25 +0530

    Create sid_sortbymethod.js

---
+++ sid_sortbymethod.js
@@ -0,0 +1,40 @@
+function sidsortfunction(parametersortby){
+    // sid sort method start
+    if(parametersortby == 'low'){
+      this.sid_sortby.sort(function(a, b) { return a.variantSalePrice - b.variantSalePrice; })
+    }
+    else if(parametersortby == 'high'){
+      this.sid_sortby.sort(function(a, b) { return b.variantSalePrice - a.variantSalePrice; })
+    }
+    else if(parametersortby == 'A'){
+      this.sid_sortby.sort(function(a, b) {
+        var nameA = a.variantName.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.variantName.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        // names must be equal
+        return 0;
+      });
+
+    }
+    else if(parametersortby == 'Z'){
+      this.sid_sortby.reverse(function(a, b) {
+        var nameA = a.variantName.toUpperCase(); // ignore upper and lowercase
+        var nameB = b.variantName.toUpperCase(); // ignore upper and lowercase
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+      
+        // names must be equal
+        return 0;
+      });
+
+    }
+    // sid sort method end
